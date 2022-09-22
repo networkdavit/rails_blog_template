b51f1275863d9ec3eca4025249769bde09badfa7
networkdavit <lonecreation1612@gmail.com>
2022-09-22 15:10:45 +0400

    Changes in style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
     def authorize
         if !current_user
-          flash[:alert] = "You aren't authorized to visit that page."
+          flash[:notice] = "You aren't authorized to visit that page."
           redirect_to '/'
         end
     end
