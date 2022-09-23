a9957c0079fdaf6f1f1180f3ac15f159e711775d
networkdavit <lonecreation1612@gmail.com>
2022-09-23 23:48:40 +0400

    Rich text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
+    has_rich_text :body
     def to_param  # overridden
         title
     end
     validates :title, presence: true, length: {minimum: 5, maximum: 50}
-    validates :body, presence: true, length: {minimum: 10, maximum: 1000}
+    validates :body, presence: true, length: {minimum: 10, maximum: 15000}
 end
